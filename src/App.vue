1368d9a7b1a3022bd2cab1f712372a64c7076edc
tatawang <525916454@qq.com>
2022-08-26 23:49:08 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+	<Demo01></Demo01>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Demo01 from './components/Demo01.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+	Demo01
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Demo01.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<h1 id="demo">{{name}}</h1>
+		<button @click="info()">点我显示</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Demo01',
+		data() {
+			return {
+				name: 'zhangsan',
+				age:18,
+				
+			}
+		},
+		computed:{
+			show(){
+				
+				return this.name + '今年' + this.age
+			}
+		},
+		methods:{
+			info(){
+				alert(this.show)
+			}
+		}
+	}
+</script>
+
+<style>
+	#demo{
+		font-size: 40px;
+		background-color: aquamarine;
+	}
+</style>
